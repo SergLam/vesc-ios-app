5bee2112b88d9b7a13552abd177b0cf5f686c05e
Rene Sijnke <renesijnke@gmail.com>
2017-02-26 12:23:13 +0100

    Initial Commit

---
+++ vesc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vesc
+//
+//  Created by Rene Sijnke on 26/02/2017.
+//  Copyright © 2017 Rene Sijnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vesc/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  vesc
+//
+//  Created by Rene Sijnke on 26/02/2017.
+//  Copyright © 2017 Rene Sijnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
