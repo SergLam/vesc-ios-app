bb6ac0c96c04f5c730844c7d8c862e00723a27a2
Rene Sijnke <renesijnke@gmail.com>
2017-02-26 13:37:52 +0100

    Setting up bluetooth connection

--- vesc/SettingsViewController.h
+++ vesc/SettingsViewController.h
@@ -8,8 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SettingsViewController : UIViewController
+@import CoreBluetooth;
+@import QuartzCore;
 
+#define UART_SERVICE_UUID @"6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+#define RX_CHARACTERISTIC_UUID @"6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define TX_CHARACTERISTIC_UUID @"6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define DEVICE_INFO_UUID @"180A"
+#define HARDWARE_REVISION_UUID @"2A27"
+
+@interface SettingsViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral     *vescPeripheral;
+
+@property (nonatomic, strong) CBCharacteristic *txCharacteristic;
+@property (nonatomic, strong) CBCharacteristic *rxCharacteristic;
 
 @end
 
